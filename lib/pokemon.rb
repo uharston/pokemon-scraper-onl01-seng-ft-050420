091b977b300c949abee1e4d63b33963bc196d613
Uriah Harston <uharston@gmail.com>
2020-06-03 22:31:35 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,36 +12,8 @@ class Pokemon
   # end
 
   def initialize (id:, name:, type:, db:)
-  @id = id
-  @name = name
-  @type = type
-  @db = db
-
-end
-
-  def self.save(name, type, db)
-    sql = <<-SQL
-      INSERT INTO pokemon (name, type) VALUES (?,?)
-    SQL
-
-    db.execute(sql, name, type)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
-
-  def self.find(id, db)
-    sql = <<-SQL
-      SELECT *
-      FROM pokemon
-      WHERE id = ?
-    SQL
-
-    row = db.execute(sql, id).first
-    self.new(id:row[0], name:row[1], type:row[2], db:row[3])
-    # binding.pry
-    # db.execute(sql, id).map do |row|
-    #   self.new(row)
-    # end.first
-
-
-  end
-
-end
