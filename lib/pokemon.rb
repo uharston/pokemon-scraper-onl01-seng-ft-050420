7bab8e466492f8d8407f2c504ead50a24eb66920
Uriah Harston <uharston@gmail.com>
2020-06-03 22:34:20 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,4 +16,26 @@ class Pokemon
     @name = name
     @type = type
     @db = db
+
+end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?,?)
+    SQL
+
+    db.execute(sql, name, type)
   end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+
+    row = db.execute(sql, id).first
+    self.new(id:row[0], name:row[1], type:row[2], db:row[3])
+  end
+
+end
